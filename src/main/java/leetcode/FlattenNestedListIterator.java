20c1dcca1cc1fa892c35e117963d7db08ccd8fa3
Artur M <artyrillo@yandex.ru>
2023-05-18 21:34:00 +0300

    some leetcode

---
+++ src/main/java/leetcode/FlattenNestedListIterator.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FlattenNestedListIterator {
+    /**
+     * Leetcode 341
+     * You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements
+     * may also be integers or other lists. Implement an iterator to flatten it.
+     * Implement the NestedIterator class:
+     * NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+     * int next() Returns the next integer in the nested list.
+     * boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+     */
+    static class NestedIterator implements Iterator<Integer> {
+        private List<Integer> ints = new ArrayList<>();
+        private int position;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            flattenList(nestedList);
+        }
+
+    private void flattenList(List<NestedInteger> nestedList) {
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                ints.add(nestedInteger.getInteger());
+            } else {
+                flattenList(nestedInteger.getList());
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < ints.size();
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return ints.get(position++);
+    }
+
+    public interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return empty list if this NestedInteger holds a single integer
+        List<NestedInteger> getList();
+    }
+}
+}
---
+++ src/main/java/leetcode/LineReflection.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LineReflection {
+    /**
+     * Leetcode 356
+     * Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points
+     * symmetrically.
+     * In other words, answer whether or not if there exists a line that after reflecting all points over the given line,
+     * the original points' set is the same as the reflected ones.
+     */
+    public boolean isReflected(int[][] points) {
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        Set<Point> pointSet = new HashSet<>();
+        for (int[] point : points) {
+            minX = Math.min(minX, point[0]);
+            maxX = Math.max(maxX, point[0]);
+            pointSet.add(new Point(point[0], point[1]));
+        }
+        int sum = minX + maxX;
+        for (int[] point : points) {
+            if (!pointSet.contains(new Point(sum - point[0], point[1]))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    record Point (int x, int y) {}
+}
