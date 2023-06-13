71f968aa345700939f27368703ed5f7233b6c23e
Artur M <artyrillo@yandex.ru>
2023-06-13 16:53:58 +0300

    leetcode

--- src/main/java/leetcode/ImplementStrStr.java
+++ src/main/java/leetcode/ImplementStrStr.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-
 public class ImplementStrStr {
     /**
      * Leetcode 28
---
+++ src/main/java/leetcode/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class MaximumSubarray {
+    /**
+     * Leetcode 53
+     * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+     */
+    static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
