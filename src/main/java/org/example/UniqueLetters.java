38a232f9907e6c4c357cc553214ffca7a0ed8cc1
Artur M. <artyrillo@yandex.ru>
2023-09-12 01:30:14 +0300

    just training

--- src/main/java/org/example/UniqueLetters.java
+++ src/main/java/org/example/UniqueLetters.java
@@ -1,18 +1,6 @@
 package org.example;
 
-import org.w3c.dom.ls.LSOutput;
+public class UniqueLetters {
 
-import java.util.*;
 
-public class UniqueLetters extends AcidAndrew {
-
-    public static void addss() {
-        UniqueLetters ul = new UniqueLetters();
-        System.out.println(ul instanceof AcidAndrew);
-
-    }
-
-    public static void main(String[] args) {
-        addss();
-    }
 }
